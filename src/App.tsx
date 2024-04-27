c8a1402d21bde68f40ce454c67b0b28b4e3d5e67
Yobuligo <hoffmannpeter82@gmail.com>
2024-04-27 21:46:38 +0200

    Implement next and previous

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,16 @@ import { IImage } from "./model/IImage";
 
 export const App: React.FC = () => {
   const [images] = useState<IImage[]>([
-    { content: "first" },
-    { content: "second" },
-    { content: "third" },
+    { content: "1" },
+    { content: "2" },
+    { content: "3" },
+    { content: "4" },
+    // { content: "5" },
+    // { content: "6" },
   ]);
   return (
     <div className={styles.app}>
-      <Carousel images={images} />;
+      <Carousel images={images} />
     </div>
   );
 };
--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -1,8 +1,54 @@
+import { useState } from "react";
+import { ReactComponent as LeftArrow } from "../../assets/arrow-left.svg";
+import { ReactComponent as RightArrow } from "../../assets/arrow-right.svg";
 import { Image } from "../image/Image";
 import styles from "./Carousel.module.scss";
 import { ICarouselProps } from "./ICarouselProps";
 
 export const Carousel: React.FC<ICarouselProps> = (props) => {
-  const items = props.images.map((image) => <Image image={image} />);
-  return <div className={styles.carousel}>{items}</div>;
+  const [cursor, setCursor] = useState(0);
+
+  const items = props.images
+    .filter((_, index) => index === cursor)
+    .map((image, index) => <Image key={index} image={image} />);
+
+  const onNext = () => {
+    setCursor((previous) => {
+      // contains only one item?
+      if (props.images.length === 1) {
+        return previous;
+      }
+
+      // At the end? Navigate to first
+      if (previous === props.images.length - 1) {
+        return 0;
+      }
+
+      return previous + 1;
+    });
+  };
+
+  const onPrevious = () => {
+    setCursor((previous) => {
+      // contains only one item?
+      if (props.images.length === 1) {
+        return previous;
+      }
+
+      // At the beginning? Navigate to last
+      if (previous === 0) {
+        return props.images.length - 1;
+      }
+
+      return previous - 1;
+    });
+  };
+
+  return (
+    <div className={styles.carousel}>
+      <LeftArrow width={"2rem"} onClick={onPrevious} />
+      {items}
+      <RightArrow width={"2rem"} onClick={onNext} />
+    </div>
+  );
 };
